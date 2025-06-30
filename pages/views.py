5e9b74d6c6b1b13b2c51f451611ebcfdf56a238e
Andrew Zigler <andrewzigler@gmail.com>
2025-06-30 09:39:29 -0700

    ✨ feat: add note tracking functionality to home page
    The commit adds a note system to track page visits, including:
    - New add_note function in models.py
    - Context data update in HomePageView
    - Display notes in home page template

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.views.generic import TemplateView
+from .models import add_note
 
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        notes = add_note("Visited home page")
+        context["notes"] = notes
+        return context
 
 
 class AboutPageView(TemplateView):
